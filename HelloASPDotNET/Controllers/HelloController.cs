b1fb0245da7509ea8f0b45e851e635f50bb2399e
B Deaz <breonaj@msn.com>
2021-07-20 09:40:45 -0400

    before checking out new branch

---
+++ HelloASPDotNET/Controllers/HelloController.cs
@@ -0,0 +1,29 @@
+﻿using Microsoft.AspNetCore.Mvc;
+using System;
+using System.Collections.Generic;
+using System.Linq;
+using System.Threading.Tasks;
+
+namespace HelloASPDotNET.Controllers
+{
+    [Route("/helloworld")]
+    public class HelloController : Controller
+    {
+          [HttpGet]
+         // [Route("/helloworld/")]
+        public IActionResult Index()
+        {
+            string html = "<form method='post'>" + "<input type='text' name='name' />" + "<input type='submit' value='Greet Me!' />" + "</form?";
+            return Content(html, "text/html");
+        }
+
+        
+        [HttpPost]
+        [Route("/hello")]
+        //[Route("helloworld/welcome")]
+        public IActionResult Welcome(string name = "World")
+        {
+            return Content("<h1>Welcome to my app, " + name + "!</h1?", "text/html");
+        }
+    }
+}
